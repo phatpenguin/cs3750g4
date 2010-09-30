9deb4fa974177c982cedb1af92ae5d75281183a1
mike_schenk@yahoo.com <Mike Schenk>
2010-09-30 15:00:14 +0000

    First UI prototypes (Blend 4 / Visual Studio 2010 solution and project files)

---
+++ RUBBBQSolution/RUBPrototypes/Startup.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RUBPrototypes
+{
+	/// <summary>
+	/// Interaction logic for Startup.xaml
+	/// </summary>
+	public partial class Startup : Window
+	{
+		public Startup()
+		{
+			this.InitializeComponent();
+			
+			// Insert code required on object creation below this point.
+		}
+
+		private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+		{
+			new MainWindow().Show();
+		}
+
+		private void Cooks_Click(object sender, System.Windows.RoutedEventArgs e)
+		{
+			new CooksScreen().Show();
+		}
+
+		private void Login_Click(object sender, System.Windows.RoutedEventArgs e)
+		{
+			new Login().Show();
+		}
+	}
+}
