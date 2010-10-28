d078dcbd8b93b33cb4bce0182b59d22988203753
mike_schenk@yahoo.com <mike_schenk@yahoo.com@63ac338b-8e5e-7440-996f-c852c0c138fa>
2010-10-28 03:01:00 +0000

    The beginning of the real project.

---
+++ trunk/BBQRMSSolution/BBQRMSSolution/Views/ChooseReport.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Controls;
+
+namespace BBQRMSSolution.Views
+{
+	public partial class ChooseReport : UserControl
+	{
+		public ChooseReport()
+		{
+			// Required to initialize variables
+			InitializeComponent();
+		}
+	}
+}
