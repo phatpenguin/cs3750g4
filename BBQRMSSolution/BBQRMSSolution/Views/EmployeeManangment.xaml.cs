0bf298f32843f2fd4f848b63c2a75f8f381546f2
jhodgkin@gmail.com <jhodgkin@gmail.com>
2010-11-02 02:57:17 +0000

    Added Employee Management screens.

---
+++ BBQRMSSolution/BBQRMSSolution/Views/EmployeeManangment.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BBQRMSSolution.Views
+{
+    /// <summary>
+    /// Interaction logic for EmployeeManangment.xaml
+    /// </summary>
+    public partial class EmployeeManangment : UserControl
+    {
+        public EmployeeManangment()
+        {
+            InitializeComponent();
+            EmployeeList.SelectionChanged += new SelectionChangedEventHandler(EmployeeList_SelectionChanged);
+            RoleList.SelectionChanged += new SelectionChangedEventHandler(RoleList_SelectionChanged);
+        }
+
+        private void RoleList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            employeeManagmentContent.Content = new RoleManagement();
+        }
+
+        private void EmployeeList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            employeeManagmentContent.Content = new EmployeeInfo();
+        }
+    }
+}
