f0a4625b71760f36b413d001cc0217f24587895f
scottwleonard@gmail.com <scottwleonard@gmail.com@63ac338b-8e5e-7440-996f-c852c0c138fa>
2010-11-03 14:45:07 +0000

    merged bbqrmssolution.csproj from rev36 added files related to ordering. ordering integration with new mainwindow not complete

---
+++ trunk/BBQRMSSolution/BBQRMSSolution/ViewModels/Menu.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace BBQRMSSolution.ViewModels
+{
+	public class Menu
+	{
+		public String name { get; set; }
+		public ObservableCollection<MenuItem> menuItems { get; set; }
+
+		public Menu()
+		{
+			menuItems = new ObservableCollection<MenuItem>();
+		}
+
+	}
+}
---
+++ trunk/BBQRMSSolution/BBQRMSSolution/ViewModels/MenuItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BBQRMSSolution.ViewModels
+{
+	public delegate void MenuItemClickHandler(object sender, EventArgs e);
+
+	public class MenuItem
+	{
+		public MenuItemClickHandler Clickr { get; set; }
+		public String name { get; set; }
+		public int id { get; set; }
+		public decimal price { get; set; }
+	}
+}
