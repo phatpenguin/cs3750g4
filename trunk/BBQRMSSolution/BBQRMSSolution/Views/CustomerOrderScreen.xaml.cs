047c14fddc767ff880123bfd1760cb4fd92e5525
scottwleonard@gmail.com <scottwleonard@gmail.com@63ac338b-8e5e-7440-996f-c852c0c138fa>
2010-11-04 01:56:17 +0000

    the order screen is mostly working.  still need to get totals and

--- trunk/BBQRMSSolution/BBQRMSSolution/Views/CustomerOrderScreen.xaml.cs
+++ trunk/BBQRMSSolution/BBQRMSSolution/Views/CustomerOrderScreen.xaml.cs
@@ -23,15 +23,5 @@ namespace BBQRMSSolution.Views
 		{
 			InitializeComponent();
 		}
-
-		private void Button_Click(object sender, RoutedEventArgs e)
-		{
-
-		}
-
-		private void Button_Click_1(object sender, RoutedEventArgs e)
-		{
-
-		}
 	}
 }
--- trunk/UIPrototypesSolution/RUBPrototypes/MainWindow.xaml.cs
+++ trunk/UIPrototypesSolution/RUBPrototypes/MainWindow.xaml.cs
@@ -23,5 +23,10 @@ namespace RUBPrototypes
 
 			// Insert code required on object creation below this point.
 		}
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			this.Close();
+		}
 	}
 }
