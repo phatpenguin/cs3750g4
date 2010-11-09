f347af6a6804750a5c6d8ae2222079993d323be9
Scott Leonard <phatpenguindev@gmail.com>
2010-11-09 02:44:24 +0000

    made the order screen work properly

--- BBQRMSSolution/BBQRMSSolution/Models/OrderItem.cs
+++ BBQRMSSolution/BBQRMSSolution/Models/OrderItem.cs
@@ -2,13 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using BBQRMSSolution.ViewModels;
 
 namespace BBQRMSSolution.Models
 {
-	public class OrderItem
+	public class OrderItem : BaseModel
 	{
 		public MenuItem menuItem { get; set; }
-		public int quantity { get; set; }
+
+		private int q;
+		public int quantity { get { return q; } set { q = value; NotifyPropertyChanged("quantity"); } }
+		public DelegateCommand DoAction { get; set; }
 
 		public OrderItem()
 		{
--- BBQRMSSolution/BBQRMSSolution/ViewModels/CustomerOrderScreenViewModel.cs
+++ BBQRMSSolution/BBQRMSSolution/ViewModels/CustomerOrderScreenViewModel.cs
@@ -10,7 +10,7 @@ namespace BBQRMSSolution.ViewModels
 	public class CustomerOrderScreenViewModel : ViewModelBase
 	{
 		public ObservableCollection<Menu> menus { get; set; }
-		public ObservableCollection<OrderItem> order { get; set; }
+		public ObservableCollection<OrderItem> order { get;set;}
 
 		private decimal st,tp,ta;
 		public decimal subTotal { get { return st; } set { st = value; NotifyPropertyChanged("subTotal"); } }
@@ -19,8 +19,12 @@ namespace BBQRMSSolution.ViewModels
 
 		private const decimal TAX_PERCENTAGE = 8.25m;
 
+		public DelegateCommand Cancel { get; private set; }
+
 		public CustomerOrderScreenViewModel()
 		{
+			Cancel = new DelegateCommand(cancelOrder);
+
 			totalPrice = 0.00m;
 			subTotal = 0.00m;
 			taxAmount = 0.00m;
@@ -31,21 +35,21 @@ namespace BBQRMSSolution.ViewModels
 				new Menu { 
 					name = "Sides", 
 					menuItems= {
-						new MenuItem {name="Fries",id=1,price=3.50m,AddItemToOrder=new DelegateCommand(addMenuItem)},	
-						new MenuItem {name="Beans",id=2,price=4.00m,AddItemToOrder=new DelegateCommand(addMenuItem)},
-						new MenuItem {name="Potato Salad",id=3,price=5.00m,AddItemToOrder=new DelegateCommand(addMenuItem)},
-						new MenuItem {name="Coleslaw",id=4,price=1.25m,AddItemToOrder=new DelegateCommand(addMenuItem)},
-						new MenuItem {name="Corn on the Cob",id=5,price=2.25m,AddItemToOrder=new DelegateCommand(addMenuItem)}
+						new MenuItem {name="Fries",id=1,price=3.50m,DoAction=new DelegateCommand(addMenuItem)},	
+						new MenuItem {name="Beans",id=2,price=4.00m,DoAction=new DelegateCommand(addMenuItem)},
+						new MenuItem {name="Potato Salad",id=3,price=5.00m,DoAction=new DelegateCommand(addMenuItem)},
+						new MenuItem {name="Coleslaw",id=4,price=1.25m,DoAction=new DelegateCommand(addMenuItem)},
+						new MenuItem {name="Corn on the Cob",id=5,price=2.25m,DoAction=new DelegateCommand(addMenuItem)}
 					}
 
 				},
 				new Menu {
 								name="Drinks",
 								menuItems={
-												new MenuItem {name="Soda",id=1,price=1.25m,AddItemToOrder=new DelegateCommand(addMenuItem)},
-												new MenuItem {name="Beer",id=2,price=2.25m,AddItemToOrder=new DelegateCommand(addMenuItem)},
-												new MenuItem {name="Bottled Water",id=3,price=1.00m,AddItemToOrder=new DelegateCommand(addMenuItem)},
-															new MenuItem {name="Sobe",id=4,price=1.99m,AddItemToOrder=new DelegateCommand(addMenuItem)}
+												new MenuItem {name="Soda",id=6,price=1.25m,DoAction=new DelegateCommand(addMenuItem)},
+												new MenuItem {name="Beer",id=7,price=2.25m,DoAction=new DelegateCommand(addMenuItem)},
+												new MenuItem {name="Bottled Water",id=8,price=1.00m,DoAction=new DelegateCommand(addMenuItem)},
+															new MenuItem {name="Sobe",id=9,price=1.99m,DoAction=new DelegateCommand(addMenuItem)}
 								}
 				}
 			};
@@ -54,19 +58,50 @@ namespace BBQRMSSolution.ViewModels
 		public void addMenuItem(Object mi)
 		{
 			MenuItem menuItem = (MenuItem)mi;
-			OrderItem orderItem = new OrderItem { menuItem = menuItem, quantity = 1 };
-			order.Add(orderItem);
+
+			bool isFound = true;
+			foreach (OrderItem oi in order)
+			{
+				isFound = true;
+				if (oi.menuItem.id == menuItem.id)
+				{
+					oi.quantity++;
+					NotifyPropertyChanged("oi");
+					break;
+				}
+				else isFound = false;
+			}
+			if (!isFound || order.Count == 0)
+			{
+				OrderItem orderItem = new OrderItem { menuItem = menuItem, quantity = 1, DoAction=new DelegateCommand(removeMenuItem) };
+				order.Add(orderItem);
+			}
+
 			subTotal += menuItem.price;
 			taxAmount = subTotal * (TAX_PERCENTAGE / (decimal)100);
 			totalPrice = subTotal + taxAmount;
 		}
 
-		public void removeMenuItem(Object mi)
+		public void removeMenuItem(Object oi)
 		{
-			MenuItem menuItem = (MenuItem)mi;
-			//TODO Logic to remove item from order.
-			subTotal -= menuItem.price;
+			OrderItem orderItem = (OrderItem)oi;
+
+			if (orderItem.quantity > 1) orderItem.quantity--;
+			else order.Remove(orderItem);
+
+			subTotal -= orderItem.menuItem.price;
+			taxAmount = subTotal * (TAX_PERCENTAGE / (decimal)100);
+			totalPrice = subTotal + taxAmount;
 		}
 
+
+		public void cancelOrder()
+		{
+			totalPrice = 0.00m;
+			subTotal = 0.00m;
+			taxAmount = 0.00m;
+
+			order.Clear();
+		}
 	}
 }
