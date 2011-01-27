d7606da64175871707c287229288142ef8dc76a2
Scott Leonard <phatpenguindev@gmail.com>
2011-01-27 09:27:23 +0000

    project builds now.  Seems like all the database issues are resolved.  menuitemmap is now set up correctly with a composite primary key.

---
+++ BBQRMSSolution/Server/BBQRMS.Database/Schema Objects/Schemas/dbo/Tables/Keys/Menu.FK_MenuItemMap_Menu.fkey.sql
@@ -0,0 +1,4 @@
+﻿ALTER TABLE [dbo].[MenuItemMap]
+	ADD CONSTRAINT [FK_MenuItemMap_Menu] 
+	FOREIGN KEY (MenuId)
+	REFERENCES Menu (Id)	
---
+++ BBQRMSSolution/Server/BBQRMS.Database/Schema Objects/Schemas/dbo/Tables/Keys/MenuItemMap.FK_MenuItem_MenuItemMap.fkey.sql
@@ -0,0 +1,4 @@
+﻿ALTER TABLE [dbo].[MenuItemMap]
+	ADD CONSTRAINT [FK_MenuItemMap_MenuItem] 
+	FOREIGN KEY (MenuItemId)
+	REFERENCES MenuItem (Id)	
