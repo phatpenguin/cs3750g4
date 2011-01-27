f523b8b8fd18d8848cf419ccbd5cd35906a3164b
jhodgkin@gmail.com <jhodgkin@gmail.com@63ac338b-8e5e-7440-996f-c852c0c138fa>
2011-01-27 03:55:32 +0000

    Added some type inserts.

---
+++ trunk/BBQRMSSolution/Server/BBQRMS.Database/Scripts/Post-Deployment/ReferenceData/InitialPaymentTypeData.sql
@@ -0,0 +1,27 @@
+﻿-- =============================================
+-- Script Template
+-- =============================================
+select top 0 *
+into #PaymentType
+from (select * from [dbo].[PaymentType]
+union select top 0 * from [dbo].[PaymentType]) a
+
+insert into #PaymentType
+values 
+(1, 'Cash'),
+(2, 'Credit Card'),
+(3, 'Debit Card')
+
+set identity_insert [dbo].[PaymentType] on
+
+update [dbo].[PaymentType]
+set Id = Source.Id,
+Descr = Source.Descr
+from [dbo].[PaymentType] Target inner join #PaymentType Source on Target.id = Source.id
+
+insert into [dbo].[PaymentType] (Id, Descr)
+select Source.*
+from [dbo].[PaymentType] Target right join #PaymentType Source on Target.id = Source.id
+where Target.id is null
+
+set identity_insert [dbo].[PaymentType] off
