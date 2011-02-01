fc0d29df07da9d9dce6d65c9a4925a2d98b635f5
jessie.floyd@gmail.com <jessie.floyd@gmail.com@63ac338b-8e5e-7440-996f-c852c0c138fa>
2011-02-01 04:19:28 +0000

    Created my InventoryManagementMenuView stub and classes

---
+++ trunk/BBQRMSSolution/Client/BBQRMS.Client/ViewModels/InventoryManagementMenuViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BBQRMSSolution.ViewModels
+{   
+    public class InventoryManagementMenuViewModel:ViewModelBase
+    {
+
+
+        public void HandleAddSupplier()
+        {
+
+
+        }
+    }
+
+   
+}
