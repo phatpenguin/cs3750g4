ac1fe8169407373d8eb754169f2e513df0f0bfce
scottwleonard@gmail.com <scottwleonard@gmail.com@63ac338b-8e5e-7440-996f-c852c0c138fa>
2011-02-27 20:55:59 +0000

    The add payment button now brings up the add payment screen. add payment screen still not functional. Cancel button on customer order screen now deletes order and creates new Order order# now calculates based on the id of the order table by doing orderid%1000 added a confirmation box view based on the clockout view.  Is there a way to add this to each screen for various confirmations? changed the menuitem table, but i dont remember what I did.

--- trunk/BBQRMSSolution/Client/BBQRMS.Client/ViewModels/CustomerOrderScreenViewModel.cs
+++ trunk/BBQRMSSolution/Client/BBQRMS.Client/ViewModels/CustomerOrderScreenViewModel.cs
@@ -9,26 +9,56 @@ namespace BBQRMSSolution.ViewModels
 	public class CustomerOrderScreenViewModel : ViewModelBase
 	{
 
+	    private bool _paymentVisible;
+
         public DelegateCommand AddToOrder { get { return new DelegateCommand(Order.AddItem); } }
 
 		public ObservableCollection<Menu> Menus { get; set; }
 		public OrderViewModel Order { get; set; }
 
-		public DelegateCommand Cancel { get { return new DelegateCommand(CancelOrder); } }
+		public DelegateCommand Cancel { get { return new DelegateCommand(Order.CancelOrder); } }
 		public DelegateCommand Cashier { get { return new DelegateCommand(PlaceOrder); } }
+        public DelegateCommand Cook { get { return new DelegateCommand(Order.SendToCook); } }
+        public DelegateCommand Payment { get { return new DelegateCommand(AddPayment); } }
+
+        public bool PaymentVisible
+        {
+            get
+            {
+                return _paymentVisible;
+            }
+            set
+            {
+                if (value != _paymentVisible)
+                {
+                    _paymentVisible = value;
+                    MessageBus.Publish(new ClockOutMode(_paymentVisible));
+                    NotifyPropertyChanged("PaymentVisible");
+                }
+            }
+        }
+
 
 		[Obsolete("Used for design-time only", true)]
 		public CustomerOrderScreenViewModel()
 		{
-		    Menu m1 = Menu.CreateMenu("FOOD", 0);
-		    Menu m2 = Menu.CreateMenu("FOOD2", 1);
-		    Menu m3 = Menu.CreateMenu("FOOD3", 2);
+		    PaymentVisible = false;
 
-            Menus = new ObservableCollection<Menu>();
-            Menus.Add(m1);
-            Menus.Add(m2);
-            Menus.Add(m3);
+		    var m1 = Menu.CreateMenu("FOOD", 0);
+		    var m2 = Menu.CreateMenu("FOOD2", 1);
+		    var m3 = Menu.CreateMenu("FOOD3", 2);
 
+		    var mi1 = new MenuItem {Description = "MenuItem1", Id = 1, Price = 1.25m};
+		    var mi2 = new MenuItem {Description = "MenuItem2", Id = 2, Price = 2.25m};
+		    var mi3 = new MenuItem {Description = "MenuItem3", Id = 3, Price = 3.25m};
+
+            m1.MenuItems.Add(mi1);
+            m2.MenuItems.Add(mi2);
+            m3.MenuItems.Add(mi3);
+
+            Menus = new ObservableCollection<Menu> {m1, m2, m3};
+
+		    Order = new OrderViewModel();
 		}
 
 		public CustomerOrderScreenViewModel(BBQRMSEntities dataService, IMessageBus messageBus)
@@ -39,22 +69,11 @@ namespace BBQRMSSolution.ViewModels
 			Order = new OrderViewModel(MessageBus,DataService);
 
 		    Menus = new ObservableCollection<Menu>(DataService.Menus.Execute());
-		    foreach(var m in Menus)
-		    {
+		    foreach (var m in Menus)
 		        DataService.LoadProperty(m, "MenuItems");
-		    }
 
-		    foreach (Menu menu in Menus)
-		    {
+		    foreach (var menu in Menus)
 		        DataService.LoadProperty(menu, "MenuItems");
-		    }
-            
-		}
-
-		public void CancelOrder()
-		{
-			Order = new OrderViewModel(MessageBus,DataService);
-			NotifyPropertyChanged("order");
 		}
 
 		public void PlaceOrder()
@@ -66,5 +85,10 @@ namespace BBQRMSSolution.ViewModels
 
 			MessageBus.Publish(new ShowScreen(orderCashierScreenViewModel));
 		}
+
+        public void AddPayment()
+        {
+            PaymentVisible = true;
+        }
 	}
 }
--- trunk/BBQRMSSolution/Client/BBQRMS.Client/ViewModels/PaymentViewModel.cs
+++ trunk/BBQRMSSolution/Client/BBQRMS.Client/ViewModels/PaymentViewModel.cs
@@ -1,13 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
+using BBQRMSSolution.ServerProxy;
+using Controls;
 
 namespace BBQRMSSolution.ViewModels
 {
 	class PaymentViewModel : ViewModelBase
 	{
+        public ObservableCollection<PaymentType> PaymentTypes { get; set; }
 
+        public PaymentViewModel()
+        {
+            PaymentTypes = new ObservableCollection<PaymentType>
+                               {
+                                   new PaymentType {Description = "Cash"},
+                                   new PaymentType {Description = "Credit"},
+                                   new PaymentType {Description = "Check"}
+                               };
+        }
+
+        public PaymentViewModel(BBQRMSEntities dataService, IMessageBus messageBus)
+        {
+            PaymentTypes = new ObservableCollection<PaymentType>(DataService.PaymentTypes.Execute());
+        }
 
 	}
 }
