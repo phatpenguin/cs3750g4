9941ef9702568e04a613d2e4cd0cbe62229d37fb
jhodgkin@gmail.com <jhodgkin@gmail.com>
2011-03-09 06:31:30 +0000

    I added the basic menu item crud functionality. You can now add and edit menu items.

---
+++ BBQRMSSolution/Client/BBQRMS.Client/ViewModels/MenuItemManagementViewModel.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data.Services.Client;
+using System.Linq;
+using System.Text;
+using BBQRMSSolution.ServerProxy;
+
+namespace BBQRMSSolution.ViewModels
+{
+    public class MenuItemManagementViewModel : ViewModelBase
+    {
+
+        private ObservableCollection<MenuItem> menuItems;
+        private MenuItem selectedMenuItem;
+
+        public MenuItemManagementViewModel(BBQRMSEntities dataService)
+	    {
+	    	DataService = dataService;
+
+			ResetList();
+
+	    }
+
+	    private void ResetList()
+	    {
+	        DataService.MergeOption = MergeOption.PreserveChanges;
+            MenuItems = new ObservableCollection<MenuItem>(DataService.MenuItems);
+            SelectedMenuItem = MenuItems[0];
+	    }
+
+        public ObservableCollection<MenuItem> MenuItems
+	    {
+	        get { return menuItems; }
+	        set { menuItems = value; NotifyPropertyChanged("MenuItems");}
+	    }
+
+        public MenuItem SelectedMenuItem
+	    {
+            get { return selectedMenuItem; }
+	        set
+	        {
+                selectedMenuItem = value;
+
+                NotifyPropertyChanged("SelectedMenuItem");
+            }
+	    }
+
+	    public void HandleSaveClick() {
+            if (SelectedMenuItem.Id > 0) {
+                DataService.UpdateObject(SelectedMenuItem);
+            } else {
+                DataService.AddToMenuItems(SelectedMenuItem);      
+            }
+	        DataService.SaveChanges();
+	        SelectedMenuItem = null;
+	    }
+
+        public void HandleCreateItem()
+        {
+            SelectedMenuItem = new MenuItem();
+            SelectedMenuItem.Name = "New Menu Item";
+            MenuItems.Add(SelectedMenuItem);
+        }
+
+        public void HandleDeleteMenuItem()
+        {
+            HandleSaveClick();
+            ResetList();
+        }
+
+    }
+}
