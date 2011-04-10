1f0648eb2e851527ce623e774c2e08beee12579e
scottwleonard@gmail.com <scottwleonard@gmail.com@63ac338b-8e5e-7440-996f-c852c0c138fa>
2011-04-10 21:49:30 +0000

    RESOLVED  BBQRMS-95.  Added NumericTextBox which is a wrapper for TextBox that will only allow numeric inpput,

---
+++ trunk/BBQRMSSolution/Client/Controls/NumericTextBox.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Controls
+{
+    public class NumericTextBox : TextBox
+    {
+        public NumericTextBox()
+        {
+            DataObject.AddPastingHandler(this, CheckPasteFormat);
+        }
+
+        private Boolean CheckFormat(string text)
+        {
+            short val;
+            return Int16.TryParse(text, out val);
+        }
+
+        private void CheckPasteFormat(object sender, DataObjectPastingEventArgs e)
+        {
+            var isText = e.SourceDataObject.GetDataPresent(DataFormats.Text, true);
+            if (isText)
+            {
+                var text = e.SourceDataObject.GetData(DataFormats.Text) as string;
+                if (CheckFormat(text))
+                {
+                    return;
+                }
+            }
+            e.CancelCommand();
+        }
+
+        protected override void OnPreviewTextInput(System.Windows.Input.TextCompositionEventArgs e)
+        {
+            if (!CheckFormat(e.Text))
+            {
+                e.Handled = true;
+            }
+            else
+            {
+                base.OnPreviewTextInput(e);
+            }
+        }
+    }
+}
