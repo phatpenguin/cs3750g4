833d7345586c9d2f4ad783b1ce1024a398473a04
Scott Leonard <phatpenguindev@gmail.com>
2011-04-12 02:38:47 +0000

    changed decimal places in orderitems to 2 to try to fix a money problem

--- BBQRMSSolution/Server/BBQRMS.Database/Schema Objects/Schemas/dbo/Tables/OrderItem.table.sql
+++ BBQRMSSolution/Server/BBQRMS.Database/Schema Objects/Schemas/dbo/Tables/OrderItem.table.sql
@@ -5,6 +5,6 @@
 	MenuItemId	int				NOT NULL,
 	Name		varchar(MAX)	NOT NULL,
 	Quantity	int				NOT NULL,
-	UnitPrice	numeric(18,10)	NOT NULL,
-	UnitTax		numeric(18,10)	NOT NULL
+	UnitPrice	numeric(18,2)	NOT NULL,
+	UnitTax		numeric(18,2)	NOT NULL
 )
